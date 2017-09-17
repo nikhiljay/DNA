141deaf17f84698db527902639d973c1cd0cd7fe
Nikhil D'Souza <nikhiljay7@gmail.com>
2017-09-17 12:10:23 -0700

    Web is somewhat functional using socket.io.

--- web/main.js
+++ web/main.js
@@ -5,48 +5,70 @@ var seq = require('bionode-seq')
 var ngrok = require('ngrok')
 var bodyParser = require('body-parser');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-// var image = path.resolve(__dirname, 'text.png');
 var inputStrand = ""
 var method = ""
 
+//DATA
 app.use(bodyParser.json())
 
-app.post('/data', function(req, res) {
+io.on('connection', function(socket) {
+  app.post('/data', function(req, res) {
     res.send(req.body);
 
     var data = req.body //get data from app
     inputStrand = data['strand']
     method = data['method']
+    io.emit('webStrand', {
+      'inputStrand': inputStrand
+    });
     console.log("-------------")
     console.log("input strand: " + inputStrand)
     console.log("method: " + method)
+  });
+
+  app.get('/data', function(req, res) {
+    var outputStrand = {
+      "outputStrand": seq.complement(inputStrand)
+    }
+
+    switch (method) {
+      case "Reverse":
+        outputStrand = {
+          "outputStrand": seq.reverse(inputStrand)
+        }
+        break;
+      case "Complement":
+        outputStrand = {
+          "outputStrand": seq.complement(inputStrand)
+        }
+        break;
+      case "Transcribe":
+        outputStrand = {
+          "outputStrand": seq.transcribe(inputStrand)
+        }
+        break;
+      case "Translate":
+        outputStrand = {
+          "outputStrand": seq.translate(inputStrand)
+        }
+        break;
+    }
+
+    console.log("output strand: " + outputStrand['outputStrand'])
+    res.send(outputStrand) //send data to app
+    io.emit('webStrand', outputStrand);
+  });
 });
 
+//WEB
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/data', function(req, res) {
-    var outputStrand = {"strand": seq.complement(inputStrand)}
-
-    switch(method) {
-      case "Reverse":
-          outputStrand = {"strand": seq.reverse(inputStrand)}
-          break;
-      case "Complement":
-          outputStrand = {"strand": seq.complement(inputStrand)}
-          break;
-      case "Transcribe":
-          outputStrand = {"strand": seq.transcribe(inputStrand)}
-          break;
-      case "Translate":
-          outputStrand = {"strand": seq.translate(inputStrand)}
-          break;
-    }
-
-    console.log("output strand: " + outputStrand['strand'])
-    res.send(outputStrand) //send data to app
+// app.listen(8000);
+server.listen(8000, function() {
+  console.log('listening on *:8000');
 });
-
-app.listen(8000);
